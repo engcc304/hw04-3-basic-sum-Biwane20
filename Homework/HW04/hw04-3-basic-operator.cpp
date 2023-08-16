bab7c0cad94366d60fcd70c60be651e0f1576bc5
Biwane20 <biwbew20@gmail.com>
2023-08-16 22:36:32 +0700

    HW4.3

--- Homework/HW04/hw04-3-basic-operator.cpp
+++ Homework/HW04/hw04-3-basic-operator.cpp
@@ -1,7 +1,7 @@
 
 /*
     จงกรอกหมายเลขทั้งหมดจำนวน 3 ค่า และแสดงผลรวมดังแสดงดังผลลัพธ์ด้านล่าง
-    
+
     Test case:
         2 3 1
     Output:
@@ -13,3 +13,14 @@
         Answer = 6
 */
 #include <stdio.h>
+int main()
+{
+    int num1, num2, num3;
+    printf("Please enter three nubmers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    int sum = num1 + num2 + num3;
+    printf("Answer = %d\n", sum);
+
+    return 0;
+}
